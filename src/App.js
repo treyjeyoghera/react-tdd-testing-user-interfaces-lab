e83ce2cab1159f2a0ef5cb5c1fe68b15fbe0f537
treyjeyoghera <treyjeyogheraeugene@gmail.com>
2024-05-06 09:25:08 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
+
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <main>
+      <h1>Hi, I'm Treyjey</h1>
+      <img alt="My profile pic" src="../IMG-20240228-WA0007.jpg" />
+      <h2>About Me</h2>
+      <p>
+     I am 20 years old i am from ngari located in west of Nairobi
+      </p>
+      <div>
+        <a href="https://github.com">GitHub</a>
+        <a href="https://linkedin.com">LinkedIn</a>
+      </div>
+    </main>
+  );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
